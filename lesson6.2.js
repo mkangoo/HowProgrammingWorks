f05d5e1679017670b0536ab7faffbb7f77336e0f
mkangoo <mkangoo24@gmail.com>
2023-11-02 15:47:27 +0300

    objects

--- lesson6.2.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const add = (a, b) => a + b;
-
-console.log("Add number: 5 + 2 = " + add(5, 2));
-console.log("Add float: 5 + 2 = " + add(5.1, 2.3));
-console.log('Concatenate: \'5\' + \'2\' = ' + add('5', '2'));
-console.log("Subtraction: 5 + (-2) = " + add(5, -2));
