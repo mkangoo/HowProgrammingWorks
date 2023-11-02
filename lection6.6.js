09ff3078da1dcdba5575872594c414d23c2baa86
mkangoo <mkangoo24@gmail.com>
2023-11-02 18:22:27 +0300

    new shortName

--- lection6.6.js
+++ lection6.6.js
@@ -1,19 +1,19 @@
-"use strict";
-const SALUTATION = "Ave";
+'use strict';
+const SALUTATION = 'Ave';
 const colors = [
-  /* 1 */ "black",
-  /* 2 */ "red",
-  /* 3 */ "green",
-  /* 4 */ "yellow",
-  /* 5 */ "blue",
-  /* 6 */ "magenta",
-  /* 7 */ "cyan",
-  /* 8 */ "white",
+  /* 1 */ 'black',
+  /* 2 */ 'red',
+  /* 3 */ 'green',
+  /* 4 */ 'yellow',
+  /* 5 */ 'blue',
+  /* 6 */ 'magenta',
+  /* 7 */ 'cyan',
+  /* 8 */ 'white',
 ];
 const colorer = (s, color) => `\x1b[3${color}m${s}\x1b[0m`;
 const colorize = (name) => {
-  let res = "";
-  const letters = name.split("");
+  let res = '';
+  const letters = name.split('');
   let color = 1;
   for (const letter of letters) {
     res += colorer(letter, color++);
@@ -22,9 +22,12 @@ const colorize = (name) => {
   return res;
 };
 const greetings = (name) =>
-  name.includes("Augustus")
+  name.includes('Augustus')
     ? `${SALUTATION},${colorize(name)}`
     : `Hello, ${name}!`;
 
-const fullName = "Marcus Aurelius Antoninus Augustus ";
+const fullName = 'Marcus Aurelius Antoninus Augustus';
 console.log(greetings(fullName));
+
+const shortName = 'Marcus Aurelius'
+console.log(greetings(shortName));
