10275745a2221a3b5f01fdbd9f8024de2078fec1
mkangoo <mkangoo24@gmail.com>
2023-11-03 19:27:01 +0300

    getDayNumber

---
+++ SE1/lection7.1.js
@@ -0,0 +1,21 @@
+const day = ["Monday", "Tuesday", "Wensdey", "Thursday", "Friday"];
+const getDayWithoutSwitch = (x) =>
+  x > 0 && x <= day.length ? day[x - 1] : new Error("Invalid arguments");
+
+const getDayNumber = (name) => {
+  const n = day.indexOf(name);
+  return n !== -1 ? n + 1 : new Error(`Invalid day name ${name}`);
+};
+console.log(getDayWithoutSwitch(2));
+console.log(getDayNumber("Friday"));
+
+// const mas = [1, 2, 3, 4];
+// delete mas[0];
+// console.log(mas);
+
+// const animal = {
+//   age: 10,
+//   name: "Lion",
+// };
+// delete animal.age;
+// console.log(animal);
