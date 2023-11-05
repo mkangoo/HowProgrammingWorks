7ab9078129463be67511163a84e6d05d043de017
mkangoo <mkangoo24@gmail.com>
2023-11-06 00:32:07 +0300

    new

--- SE1/lection8.3default.js
+++ SE1/lection8.3default.js
@@ -5,3 +5,23 @@ fnNew(2, 3, 4);
 fnNew(2, 3);
 fnNew(2);
 fnNew();
+
+function fnOld(a, b, c) {
+  b = b || "Hello";
+  c = c || 5;
+  console.dir({ a, b, c });
+}
+fnOld(2, 3, 4);
+fnOld(2, 3);
+fnOld(2);
+fnOld();
+
+function fnHash(args) {
+  args.a = args.a || [1, 2, 3];
+  args.b = args.b || "Hello";
+  args.c = args.c || 100;
+  console.dir(args);
+}
+fnHash({ a: [1, 2, 3], b: "Hi", c: 13 });
+fnHash({ b: "World" });
+fnHash({ c: 7 });
---
+++ SE1/lection8.4rest.js
@@ -0,0 +1,5 @@
+'use strict'
+const f1 = function (...args) {
+    console.log(args);
+}
+f1(1, 2, 3);
