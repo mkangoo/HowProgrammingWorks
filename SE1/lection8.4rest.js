6b9fd4ebc88fcfecb54d9876f7279901c0c3dc78
mkangoo <mkangoo24@gmail.com>
2023-11-06 16:05:01 +0300

    new

--- SE1/lection8.4rest.js
+++ SE1/lection8.4rest.js
@@ -1,5 +1,18 @@
-'use strict'
+"use strict";
 const f1 = function (...args) {
-    console.log(args);
-}
+  console.log(args);
+};
 f1(1, 2, 3);
+
+const f2 = (...args) => {
+  args.forEach((arg) => {
+    const type = typeof arg;
+    console.log("Type " + type);
+    if (type === "object") {
+      console.log("Value " + JSON.stringify(arg));
+    } else {
+      console.log("Value: " + arg);
+    }
+  });
+};
+f2(1, "Marcus", { field: "value" });
