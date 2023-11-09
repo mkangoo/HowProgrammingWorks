103d4f5984550054f849512f5cd313c769f69062
mkangoo <mkangoo24@gmail.com>
2023-11-09 16:33:32 +0300

    new

---
+++ SE1/lection9.5forIn.js
@@ -0,0 +1,22 @@
+const hash = {
+  first: 7,
+  second: 10,
+  third: 3,
+  fourth: 9,
+};
+hash.fifth = 2;
+
+for (const key in hash) {
+  const value = hash[key];
+  console.log(key, "\t", typeof key, "\t", value, "\t", typeof value);
+}
+
+const arr = [1, 2, 3, 4, 5];
+for (const i in arr) {
+  const value = arr[i];
+  console.log(i, typeof i, value);
+}
+for (const i of arr) {
+  const value = arr[i];
+  console.log(i, typeof i, value);
+}
---
+++ SE1/lection9.6matrix.js
@@ -0,0 +1,19 @@
+const mas = [
+  [7, 10, 1, 5, 2],
+  [6, 8, 9, 4, 30],
+  [2, 9, 4, 3, 20],
+  [9, 80, 1, 1, 0],
+];
+const max = (a, b) => (a > b ? a : b);
+const res = mas
+  .map((row) => row.reduce(max))
+  .reduce((acc, rowMax) => acc + rowMax);
+console.log(res);
+
+for (const i in mas) {
+  const row = mas[i];
+  for (const j in row) {
+    const col = row[j];
+    console.log(i, j, col);
+  }
+}
