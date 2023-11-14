e9ae23fe305f5549b039ee4a2853be99b7d93a8b
mkangoo <mkangoo24@gmail.com>
2023-11-14 23:59:01 +0300

    new

---
+++ SE1/lection9.9class.js
@@ -0,0 +1,29 @@
+"use strict";
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  move(x, y) {
+    this.x += x;
+    this.y += y;
+  }
+  toString() {
+    return [`${this.x},{this.y}`];
+  }
+  static from(obj) {
+    const { x, y } = obj;
+    return new Point(x, y);
+  }
+}
+console.log(Point.prototype);
+console.log(Point.prototype.move.prototype);
+console.log(Point.constructor.prototype);
+console.log(Point.prototype.constructor.prototype);
+
+const p1 = new Point(10, 20);
+p1.move(5, 5);
+
+console.log(p1);
+console.log(p1, toString());
