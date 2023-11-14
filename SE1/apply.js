370d79d001d0558e541ee1b8aab1c83c0f397e82
mkangoo <mkangoo24@gmail.com>
2023-11-14 15:22:41 +0300

    new

---
+++ SE1/apply.js
@@ -0,0 +1,9 @@
+function sum(a, b, c) {
+  return `${this.name}: ${a + b + c}`;
+}
+
+const sumInfo = {
+  name: "sum",
+};
+
+console.log(sum.apply(sumInfo, [2, 3, 4]));
---
+++ SE1/call.js
@@ -0,0 +1,9 @@
+function sum(a, b, c) {
+  return `${this.name}: ${a + b + c}`;
+}
+
+const sumInfo = {
+  name: "sum",
+};
+
+console.log(sum.call(sumInfo, 1, 2, 3));
