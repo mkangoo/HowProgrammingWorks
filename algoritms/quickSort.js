58daa22b94a1b9368e2e7520560109cabe2377d8
mkangoo <mkangoo24@gmail.com>
2023-11-15 00:55:15 +0300

    new

--- algoritms/quickSort.js
+++ algoritms/quickSort.js
@@ -18,3 +18,21 @@ const quickSort = (arr) => {
 };
 
 console.log(quickSort(mas));
+
+// const arr = [2, 3, 1, 2, 4, 9, 8, 0, 7, 65, 4, 3];
+
+// const quickSort = (arr) => {
+//   const lgth = arr.length;
+//   if (lgth < 2) return arr;
+//   const pivotIndex = Math.floor(lgth / 2);
+//   const pivot = arr[pivotIndex];
+//   const less = [];
+//   const greater = [];
+//   arr.forEach((element, index) => {
+//     if (index !== pivotIndex) {
+//       element <= pivot ? less.push(element) : greater.push(element);
+//     }
+//   });
+//   return [...quickSort(less), pivot, ...quickSort(greater)];
+// };
+// console.log(quickSort(arr));
