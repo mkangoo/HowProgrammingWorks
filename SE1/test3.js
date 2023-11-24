be79d4bb7973782993fb6c8621253dcd5b62288a
mkangoo <mkangoo24@gmail.com>
2023-11-25 02:55:17 +0300

    new

--- SE1/test3.js
+++ SE1/test3.js
@@ -1,5 +1,23 @@
-const removeChar = (str) => {
-  const result = str.split('').pop().shift();
+function Animal(name) {
+  this.name = name;
+}
+
+Animal.prototype.makeSound = function () {
+  console.log('Generic animal sound');
 };
 
-console.log(removeChar('Hello'));
+function Dog(name, breed) {
+  Animal.call(this, name);
+  this.breed = breed;
+}
+
+// Наследование прототипа Animal
+Dog.prototype = Object.create(Animal.prototype);
+
+// Переопределение метода
+Dog.prototype.makeSound = function () {
+  console.log('Woof!');
+};
+
+const dog1 = new Dog('Buddy', 'Labrador');
+dog1.makeSound(); // Вывод: Woof!
