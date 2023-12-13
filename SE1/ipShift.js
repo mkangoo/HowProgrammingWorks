752e2425280355fb2fc8f3106decaf3fe5d03452
mkangoo <mkangoo24@gmail.com>
2023-12-14 02:35:54 +0300

    new

---
+++ SE1/ipShift.js
@@ -0,0 +1,10 @@
+const ip = (ipv) => {
+  const arr = ipv.split('.').map(Number);
+  const shift = arr.map((element, i) => element << (8 * (3 - i)));
+  const result = shift.reduce((acc, val) => acc + val);
+  return result;
+};
+
+const user = '127.0.0.1';
+
+console.log(ip(user));
