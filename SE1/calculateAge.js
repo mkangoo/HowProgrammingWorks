180fbdda9fe5db0298dc793c9914824511ed2183
mkangoo <mkangoo24@gmail.com>
2024-01-05 03:54:23 +0300

    add: function calculate age

---
+++ SE1/calculateAge.js
@@ -0,0 +1,11 @@
+const calculateAge = birthdate => {
+  const currentDate = new Date()
+  const birthDate = new Date(birthdate)
+  const ageDifference = currentDate - birthDate
+  const millisecondsInYear = 1000 * 60 * 60 * 24 * 365.25
+  const age = Math.floor(ageDifference / millisecondsInYear)
+  return age
+}
+const birthdate = '2001-08-14'
+const age = calculateAge(birthdate)
+console.log(age)
