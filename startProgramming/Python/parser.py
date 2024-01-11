a487a6acddf796e7f21f750e66f262b06541910c
mkangoo <mkangoo24@gmail.com>
2024-01-12 01:46:10 +0300

    add parser html

---
+++ startProgramming/Python/parser.py
@@ -0,0 +1,28 @@
+import sys
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://tacitcoast.github.io/'
+
+try:
+    response = requests.get(url)
+    response.raise_for_status()  # Проверка наличия ошибок в запросе
+
+    # Передаем HTML-код страницы в BeautifulSoup
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    # Открываем файл для записи (если файл не существует, он будет создан)
+    with open('output.html', 'w', encoding='utf-8') as f:
+        # Перенаправляем стандартный вывод в файл
+        original_stdout = sys.stdout
+        sys.stdout = f
+
+        try:
+            # Выводим красиво отформатированный HTML
+            print(soup.prettify())
+        finally:
+            # Восстанавливаем стандартный вывод
+            sys.stdout = original_stdout
+
+except requests.exceptions.RequestException as e:
+    print(f"Ошибка при запросе: {e}")
