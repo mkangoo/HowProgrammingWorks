bc04fe6eafd5beb1c4cbf77fa42287b0683046b4
mkangoo <mkangoo24@gmail.com>
2024-02-21 21:21:19 +0300

    chore: methods array

---
+++ JavaScript/arrayMethods/arrayOf.js
@@ -0,0 +1,7 @@
+const basket = Array.of(3)
+
+console.log(basket)
+
+const basketTwo = Array.of(3, 2)
+
+console.log(basketTwo)
---
+++ JavaScript/arrayMethods/newArray.js
@@ -0,0 +1,7 @@
+const basket = new Array(3)
+
+console.log(basket)
+
+const basketTwo = new Array(3, 2)
+
+console.log(basketTwo)
